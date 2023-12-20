749ddbc39b4cc34d74222fae15988da217d79d63
YuvalLevi03 <154444159+YuvalLevi03@users.noreply.github.com>
2023-12-20 21:15:53 +0200

    Update LinearEq.java

--- LinearEq.java
+++ LinearEq.java
@@ -6,11 +6,11 @@
  */
 public class LinearEq {
 	public static void main(String[] args) {
-	  double a = Double.parseDouble(args[0]);
-	  double b = Double.parseDouble(args[1]);
-	  double c = Double.parseDouble(args[2]);
+	        double a = Double.parseDouble(args[0]);
+	        double b = Double.parseDouble(args[1]);
+	        double c = Double.parseDouble(args[2]);
 
-          System.out.println(a + " * x + " + b + " = " + c);
-          System.out.println("x = " + (c - b) / a);
+                System.out.println(a + " * x + " + b + " = " + c);
+                System.out.println("x = " + (c - b) / a);
+       }
 }
-	}
