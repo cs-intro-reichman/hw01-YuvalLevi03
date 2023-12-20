9bdd6dcdb1e6e26bfb900fb724e3f16d6bd39035
YuvalLevi03 <154444159+YuvalLevi03@users.noreply.github.com>
2023-12-20 17:09:48 +0200

    Update LinearEq.java

--- LinearEq.java
+++ LinearEq.java
@@ -6,7 +6,6 @@
  */
 public class LinearEq {
 	public static void main(String[] args) {
-
 	  double a = Double.parseDouble(args[0]);
 	  double b = Double.parseDouble(args[1]);
 	  double c = Double.parseDouble(args[2]);
